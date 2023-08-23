06d7f1617ce204278fd2d1cbcc4fc8e1cb6d1048
Cadame01 <89220385+Cadame01@users.noreply.github.com>
2023-08-23 05:45:22 +0000

    updated

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ let myhttp = require("http");
 
 //create server takes some arguments 
 //function (block of code that will be runed whenever the server recives http requests)
-let myserver = myhttp.createServer(
+let theserver = thehttp.createServer(
   // createServer() uses our function to run when a request comes in 
-  function( myrequest, myresponse ) {
-   console.log( myrequest.url );
+  function( therequest, theresponse ) {
+   console.log( therequest.url );
 
 
 
-   let mytext;
-   if ( myrequest.url === "/hey" ) {
-    mytext = "Well hello there...";
+   let thetext;
+   if ( therequest.url === "/hello" ) {
+    thetext = "Darkness, my old friend...";
    } else {
-     mytext = "I don't know you!";
+     thetext = "I've come to talk with you again...";
    }
 
-   mytext = mytext + "- Please help me... I am trapped in a Node.js server!";
+   thetext = thetext + "Because a vision softly creeping... Left its seeds while I was sleeping... And the vision that was planted in my brain... Still remains... Within the sound of silence.";
    
 // creates an http response header, including status code and content type
-   myresponse.writeHead( 200, { "Content-Type": "text/plain"} );
+   theresponse.writeHead( 200, { "Content-Type": "text/plain"} );
 
    // end() returns data and closes the response (sends it)
-   myresponse.end( mytext + "\n" );
+   theresponse.end( thetext );
   }
 
 );
 
 // ask http to start listening  on tcp port for incoming http requests 
 // listen() takes 2 arguments: 1: tcp prt #, string of the ip address to listen (0.0.0.0)
-myserver.listen(8080, "0.0.0.0");
-console.log("server has started");
+theserver.listen(8080, "0.0.0.0");
+console.log("the server has started");
 
